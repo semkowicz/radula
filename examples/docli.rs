1d6a2edad0c017982d4f1e53189bd37713ac33cd
Daniel Semkowicz <daniel@semkowicz.pl>
2025-04-10 22:39:31 +0200

    Add docli example

---
+++ examples/docli.rs
@@ -0,0 +1,60 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use radula::request::{RequestParameters, vesperae};
+use reqwest::Url;
+use reqwest::blocking::Request;
+use time::{Date, format_description};
+
+/// Divinum Officium Command Line Interface
+#[derive(Parser, Debug)]
+#[command(about, long_about = None)]
+struct Args {
+    /// Day for which to print Officium (e.g. 2025-02-05)
+    #[arg(short, long, value_parser = parse_date)]
+    date: Date,
+}
+
+fn parse_date(date_str: &str) -> Result<Date> {
+    let date_format = "[year]-[month]-[day]";
+    let format = format_description::parse(date_format)
+        .context("Failed to parse date format description")?;
+    let date = Date::parse(date_str, &format)
+        .with_context(|| format!("Invalid date format. Expected {date_format}."))?;
+
+    Ok(date)
+}
+
+fn construct_request(params: RequestParameters) -> Result<Request> {
+    let host = Url::parse("http://127.0.0.1:8080")
+        .context("Failed to parse Divinum Officium server URL")?;
+
+    let url = host
+        .join(params.path())
+        .context("Failed to parse target URL")?;
+
+    reqwest::blocking::Client::new()
+        .get(url)
+        .query(&params.query_pairs())
+        .build()
+        .context("Failed to build request")
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let params = vesperae(args.date).context("Failed to create request parameters")?;
+    let request = construct_request(params).context("Failed to construct request")?;
+    let html = reqwest::blocking::Client::new()
+        .execute(request)
+        .context("Failed to send request")?
+        .text()
+        .context("Failed to decode response text")?;
+
+    let officium =
+        radula::scraper::scrape_officium(&html).context("Failed to scrape officium page")?;
+
+    println!("{}", officium.day_name);
+    println!("{}", officium.part_name);
+
+    Ok(())
+}
