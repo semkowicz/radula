601a245b7c09a687061b84162ef23606483f38e2
Daniel Semkowicz <daniel@semkowicz.pl>
2026-01-30 20:44:20 +0100

    text_fragment: Add TranslationFragment
    TranslationFragment is a combination of two TextFragment objects.

--- src/style_parser/text_fragment.rs
+++ src/style_parser/text_fragment.rs
@@ -1,8 +1,10 @@
+use anyhow::bail;
+
 /// Font style that can be applied on the text.
 ///
 /// Style naming corresponds to the naming found in the Options tab from the Divinum Officium
 /// project.
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum FontStyle {
     DefaultFont,
     SmallText,
@@ -38,11 +40,51 @@ impl TextFragment {
     pub fn text(&self) -> &str {
         &self.text
     }
+
+    pub fn is_trimmed_empty(&self) -> bool {
+        self.text.trim().is_empty()
+    }
 }
 
 impl std::fmt::Display for TextFragment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let text = self.text.as_str().replace("\n", "\\n");
         write!(f, "{:?}(\"{}\")", self.font_style, text)
+    }
+}
+
+pub struct TranslationFragment<'a> {
+    font_style: FontStyle,
+    latin: &'a str,
+    trans: &'a str,
+}
+
+impl<'a> TranslationFragment<'a> {
+    pub(crate) fn from(latin: &'a TextFragment, trans: &'a TextFragment) -> anyhow::Result<Self> {
+        if latin.font_style() != trans.font_style() {
+            bail!("Text fragments differ in style");
+        }
+
+        Ok(Self {
+            font_style: latin.font_style,
+            latin: latin.text(),
+            trans: trans.text(),
+        })
+    }
+
+    pub fn font_style(&self) -> &FontStyle {
+        &self.font_style
+    }
+
+    pub fn latin(&self) -> &str {
+        self.latin
+    }
+
+    pub fn trans(&self) -> &str {
+        self.trans
+    }
+
+    pub fn is_trimmed_empty(&self) -> bool {
+        self.latin.trim().is_empty() && self.trans.trim().is_empty()
     }
 }
