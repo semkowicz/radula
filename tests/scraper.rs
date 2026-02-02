3d6f04c977e3e750a5c6620891d037ceb1508033
Daniel Semkowicz <daniel@semkowicz.pl>
2026-02-02 21:55:26 +0100

    tests: Try to scrape all days before failing the test

--- tests/scraper.rs
+++ tests/scraper.rs
@@ -1,11 +1,23 @@
+use anyhow::Context;
 use radula::request::vesperae;
 use reqwest::Url;
 use time::{Date, Duration};
+
+fn print_error(date: &Date, err: &anyhow::Error) {
+    println!("{} ({})", date, date.weekday());
+    let chain = err.chain();
+    for (i, e) in chain.enumerate() {
+        println!("    {i}. {e}");
+    }
+}
 
 /// Scrape vespers for every day in the year 2026.
 #[test]
 fn vesperae_2026() {
     let mut date = Date::from_ordinal_date(2026, 1).expect("Date is correct");
+
+    let mut correct = 0;
+    let mut failed = 0;
 
     let host = Url::parse("http://127.0.0.1:8080").expect("URL is correct");
     let client = reqwest::blocking::Client::new();
@@ -25,10 +37,22 @@ fn vesperae_2026() {
             .text()
             .expect("Failed to parse text");
 
-        if radula::scraper::scrape_officium(&html).is_err() {
-            panic!("Scraping failed for date {date}");
-        }
+        match radula::scraper::scrape_officium(&html).context("Scraping failed") {
+            Ok(_) => {
+                correct += 1;
+            }
+            Err(e) => {
+                failed += 1;
+                print_error(&date, &e);
+            }
+        };
 
         date += Duration::days(1);
     }
+
+    let total = correct + failed;
+    println!("Total: {total}");
+    println!("Failed: {failed}");
+
+    assert_eq!(failed, 0);
 }
